23f825d2c0f7225daea727377a11ffaab943bf82
PabloRosales <rosalesjuarezpablojesus@gmail.com>
2024-06-15 15:09:38 -0600

    Prepare database

---
+++ database/migrations/2024_06_15_190525_create_companies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(table: 'users');
+            $table->foreignId('state_id')->constrained(table: 'states');
+            $table->foreignId('registered_agent_id')->nullable()->constrained('registered_agents', 'id')->onDelete('set null');
+            $table->string('name')->unique();
+            $table->enum('registered_agent_type', [1, 2])->comment('use 1 to registered agent and 2 to user entity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\State;
+use Database\Seeders\StateSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // --- Users
+        \App\Models\User::factory(4)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Pablo Rosalees',
+            'email' => 'mail@mail.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // --- States
+        $this->call(StateSeeder::class);
+
+        // --- Agents
+        \App\Models\Agent::factory()->count(2)->create([
+            'state_id' => State::where('code', 'CA')->first()->id,
+        ]);
+        \App\Models\Agent::factory()->count(2)->create([
+            'state_id' => State::where('code', 'TX')->first()->id,
+        ]);
+
+        \App\Models\Agent::factory()->count(47)->create();
+
+        // --- Companies
+        \App\Models\Company::factory()->count(5)->create();
     }
 }
