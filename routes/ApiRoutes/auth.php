e541599a8629f71bb4716ecf8b143d24d2bbf485
PabloRosales <rosalesjuarezpablojesus@gmail.com>
2024-06-17 20:52:56 -0600

    Reset branch

--- routes/ApiRoutes/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\v1\LoginController;
-use Illuminate\Support\Facades\Route;
-
-Route::post("/login", LoginController::class)->name("login")->withoutMiddleware("auth:api");
--- routes/ApiRoutes/companies.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\v1\CompanyController;
-use Illuminate\Support\Facades\Route;
-
-Route::controller(CompanyController::class)->group(function(){
-    Route::post('/', 'store')->name('store');
-    Route::put('/{company}', 'update')->name('update');
-})->name('companies.');
